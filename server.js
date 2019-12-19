e776a77d3f6802529009a09697c15a0f21162361
HannesDahl <hannes.dahlvik@gmail.com>
2019-12-19 09:21:02 +0200

    installed nodemon

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.redirect('/home');
-})
+});
 
 app.get('/home', function (req, res) {
     res.render('pages/index');
