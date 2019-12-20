34ee4464845f19661ae7150c8097f8b062d9bbe5
HannesDahl <hannes.dahlvik@gmail.com>
2019-12-20 11:06:10 +0200

    Made category selecting work with one category

--- server.js
+++ server.js
@@ -40,6 +40,27 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/getproducts', function (req, res) {
+    let db = new sqlite3.Database('products.db', sqlite3.OPEN_READONLY, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the products database.');
+    });
+
+    db.serialize(() => {
+        db.all(`SELECT * FROM products`, (err, products) => {
+            if (err) console.error(err.message);
+
+            res.json(products);
+        });
+    });
+    db.close((err) => {
+        if (err) console.error(err.message);
+        console.log('Close the database connection.');
+    });
+});
+
 app.get('/addproduct', function (req, res) {
     let db = new sqlite3.Database('products.db', sqlite3.OPEN_READONLY, (err) => {
         if (err) console.error(err.message);
@@ -76,15 +97,16 @@ app.post('/addproduct', function (req, res) {
             if (err) console.error(err);
 
             console.log(`A row has been inserted with rowid ${this.lastID}`);
+
+            let categoriesvals = [this.lastID, req.fields.check];
+            db.run(`INSERT INTO product_categories (product_id, category_id) VALUES(?, ?)`, categoriesvals, function (err) {
+                if (err) console.error(err.message);
+                console.log(`A row has inserted with rowid ${this.lastID}`);
+            });
+
             res.json({
                 answer: `Product name: ${req.fields.name}, Price: ${req.fields.price}, Description: ${req.fields.description}, Thumbnail: ${req.files.image.path.replace(/^.*[\\\/]/, '', /["']/g, "")}`
             });
-        });
-
-        let categoriesvals = [null, req.fields.check];
-        db.run(`INSERT INTO product_categories (product_id, category_id) VALUES(?, ?)`, categoriesvals, function (err) {
-            if (err) console.error(err.message);
-            console.log(`A row has inserted with rowid ${this.lastID}`);
         });
     });
 });
