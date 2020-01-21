8bec229821e84637823c01bb765ab725510aa6e0
HannesDahl <hannes.dahlvik@gmail.com>
2020-01-21 13:59:51 +0200

    added firebase auth and firebase-tools and google login

---
+++ public/js/firebase.js
@@ -0,0 +1,15 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAidsNoTnXnCoz6RcZjOsJGkYXhC98GSoA",
+    authDomain: "webshop-b7847.firebaseapp.com",
+    databaseURL: "https://webshop-b7847.firebaseio.com",
+    projectId: "webshop-b7847",
+    storageBucket: "webshop-b7847.appspot.com",
+    messagingSenderId: "137923746002",
+    appId: "1:137923746002:web:c98499d500aa815dbcb50b",
+    measurementId: "G-K7Z82KTJ66"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+console.log(firebase.auth().currentUser);
