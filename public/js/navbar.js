a7bd64c8681520e388a69c9259c59a46942e1288
OliverSjolind <oliversjolind@gmail.com>
2020-01-22 09:50:05 +0200

    made so you can log out

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -37,6 +37,10 @@ $("#search").click(function (event) {
     }
 });
 
+$('.dropdown-trigger').dropdown({
+    coverTrigger: false
+});
+
 if (document.getElementById('productWrapper') && document.getElementById('search-result-text')) {
     document.getElementById('search-result-text').innerText = `Search results for: ${window.location.href.replace(/^.*[\\\/]/, '', /["']/g, "").replace("%20", " ")}`
 }
@@ -44,14 +48,28 @@ if (document.getElementById('productWrapper') && document.getElementById('search
 let signedout = document.getElementById('signedout');
 let signedIn = document.getElementById('signedIn');
 let userNameDisplay = document.getElementById('userName');
+
+function googleSignout() {
+    firebase.auth().signOut()
+
+        .then(function () {
+            console.log('Signout Succesfull')
+        }, function (error) {
+            console.log('Signout Failed')
+        });
+}
+
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         signedout.style.display = 'none';
-        signedIn.style.display = '';
+        signedIn.style.display = 'flex';
         let userFirstName = user.displayName.replace(/ .*/, '')
         userNameDisplay.innerText = userFirstName
+        let logOutButton = document.getElementById('logOut');
+        logOutButton.addEventListener('click', googleSignout)
+
     } else {
-        signedout.style.display = '';
+        signedout.style.display = 'flex';
         signedIn.style.display = 'none';
     }
 })
